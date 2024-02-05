2c12126ef072e95b6ba8918f6627fab6ef64ff06
AndreaBevilacqua <andrebevi2000@gmail.com>
2024-02-05 17:30:29 +0100

    added pokedex buttons

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -5,7 +5,29 @@ export default {
 </script>
 
 <template>
-    <main class="container"></main>
+    <main class="container">
+        <section class="pokedex">
+            <div class="pokedex-buttons d-flex gap-4 p-4">
+                <button type="button" class="btn btn-primary btn-lg"> </button>
+                <button type="button" class="btn btn-danger"></button>
+                <button type="button" class="btn btn-warning"></button>
+                <button type="button" class="btn btn-success"></button>
+            </div>
+        </section>
+    </main>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss" scoped>
+.pokedex {
+    height: 600px;
+    background-color: lightgray;
+    border-radius: 40px;
+}
+
+.pokedex-buttons {
+    background-color: grey;
+    width: 18%;
+    border-radius: 40px 0 40px 0;
+    margin-bottom: 20px;
+}
+</style>
