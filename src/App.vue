2fa53e505bd5d17fb449050112bc40930c1b2ae4
AndreaBevilacqua <andrebevi2000@gmail.com>
2024-02-05 19:22:11 +0100

    moved data api to mainapp

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,18 @@
 <script>
+import axios from 'axios';
+const endpoint = 'https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons'
 import AppMain from './components/AppMain.vue';
 export default {
   name: 'Pokemon',
-  components: { AppMain }
+  components: { AppMain },
+  data: () => ({
+    pokemons: []
+  }),
+  created() {
+    axios.get(endpoint).then(res => {
+      this.pokemons = res.data.docs;
+    })
+  }
 };
 </script>
 
--- src/components/pokemons/PokemonsList.vue
+++ src/components/pokemons/PokemonsList.vue
@@ -1,19 +1,11 @@
 <script>
-import axios from 'axios';
-const endpoint = 'https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons'
+
 import PokemonCard from './PokemonCard.vue';
 
 export default {
     name: 'PokemonsList',
-    data: () => ({
-        pokemons: []
-    }),
     components: { PokemonCard },
-    created() {
-        axios.get(endpoint).then(res => {
-            this.pokemons = res.data.docs;
-        })
-    }
+    props: { pokemons: Array }
 };
 </script>
 
