7c1b760dec2148acf5ade6bb65b94695b0e1f8b4
AndreaBevilacqua <andrebevi2000@gmail.com>
2024-02-05 17:58:52 +0100

    added pokemon card

---
+++ src/components/pokemons/PokemonCard.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    name: 'PokemonCard',
+}
+</script>
+
+<template>
+    <div class="pokemon-card">
+        <img src="" alt="">
+        <div></div>
+        <h3></h3>
+        <div></div>
+    </div>
+</template>
+
+<style lang="scss"></style>
