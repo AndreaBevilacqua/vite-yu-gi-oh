c70c6b42d141513a2dc1a6fee382da03335b0a4b
AndreaBevilacqua <andrebevi2000@gmail.com>
2024-02-05 17:54:57 +0100

    added pokemon api

--- src/components/pokemons/PokemonsList.vue
+++ src/components/pokemons/PokemonsList.vue
@@ -1,11 +1,22 @@
 <script>
+import axios from 'axios';
+const endpoint = 'https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons'
+
 export default {
     name: 'PokemonsList',
+    data: () => ({
+        pokemons: []
+    }),
+    created() {
+        axios.get(endpoint).then(res => {
+            this.pokemons = res.data.docs;
+        })
+    }
 };
 </script>
 
 <template>
-    <section id="pokeomns"></section>
+    <section id="pokemons"></section>
 </template>
 
 <style lang="scss"></style>
